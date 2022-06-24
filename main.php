0b7356022c03a6ff699c732b93cb204781970225
chan-gon <ruggedcafe@gmail.com>
2022-06-24 17:54:00 +0900

    feat: 메인 페이지 화면 구현중

--- main.php
+++ main.php
@@ -8,8 +8,73 @@
     <link rel="stylesheet" type="text/css" href="./css/main.css"/>
 </head>
 <body>
-    <?php
-        echo "Login Success";
-    ?>
+    <div class="topnav">
+        <div class="topnav-centered">
+            <a href="#home" class="active">Home</a>
+        </div>
+        <div class="topnav-right">
+            <a href="#search">홈페이지</a>
+            <a href="#about">상담접수</a>
+            <a href="#about">녹취조회</a>
+            <a href="#about">상담원 관리</a>
+            <a href="#about">코드 관리</a>
+            <a href="#about">매장 관리</a>
+            <a href="#about">블랙리스트 관리</a>
+            <a href="#about">통계</a>
+            <a href="#about">강제 로그아웃</a>
+        </div>
+    </div>
+    <div style="padding-left:16px">
+        <h3>고객정보 *  필수입력 사항</h3>
+        <div>
+            <table style="border: 1px">
+                <tr>
+                    <td>고객코드</td>
+                    <td>고객명</td>
+                    <td>ID</td>
+                    <td>회원등급</td>
+                    <td>등급 진입일</td>
+                </tr>
+                <tr>
+                    <td>전화번호</td>
+                    <td>* 핸드폰</td>
+                    <td>이메일</td>
+                </tr>
+                <tr>
+                    <td>주소</td>
+                    <td><input type="button" value="검색"></td>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <div style="padding-left:16px">
+        <h3>상담기록 *  필수입력 사항</h3>
+        <div>
+            <table style="border: 1px">
+                <tr>
+                    <td>고객코드</td>
+                    <td>고객명</td>
+                    <td>ID</td>
+                    <td>회원등급</td>
+                    <td>등급 진입일</td>
+                </tr>
+                <tr>
+                    <td>전화번호</td>
+                    <td>* 핸드폰</td>
+                    <td>이메일</td>
+                </tr>
+                <tr>
+                    <td>주소</td>
+                    <td><input type="button" value="검색"></td>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                </tr>
+            </table>
+        </div>
+    </div>
 </body>
 </html>
