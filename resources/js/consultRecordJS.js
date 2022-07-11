0e71b595676e72fa07586971a437881e3f71d03f
unknown <ruggedcafe@gmail.com>
2022-07-11 21:41:10 +0900

    Feat: #4 상담기록 로직 수정
    - 고객번호 입력값 체크와 검색 로직이 동시에 수행되는 에러 수정

--- resources/js/consultRecordJS.js
+++ resources/js/consultRecordJS.js
@@ -72,37 +72,41 @@ $("#consultRecordSaveBtn").click(function () {
        consultContent : consultContent.val()
    }
 
-    if (customerNumChk()) {
-        // 고객번호가 존재하면 상담기록 저장
-        $.ajax({
-            url: "web/addConsultRecord.php",
-            type: "POST",
-            data: formData,
-            dataType: "json",
-            success: function (data) {
-                if (data.result == 'SUCCESS') {
-                    alert("상담기록 등록 완료");
-                    $("#consult-record-form")[0].reset();
-                }
-                else if (data.result == 'FAIL') {
-                    alert(data.msg);
-                }
-                else if (data.result == 'DATA-NOTFOUND') {
-                    alert(data.msg);
-                }
-                else if (data.result == 'CONSULT_RECORD_ERROR') {
-                    alert(data.msg);
-                }
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                alert("error : " + textStatus + "\n" + errorThrown);
-            }
-        });
-    } else {
-        // 고객번호가 존재하지 않음
-        alert("존재하지 않는 고객번호 입니다.");
-        customerNum.focus();
-    }
+   if (customerNum.val() == "") {
+       alert("고객번호를 입력하세요.");
+       customerNum.focus();
+       return false;
+   }
+   else {
+       if (customerNumChk()) {
+           // 고객번호가 존재하면 상담기록 저장
+           $.ajax({
+               url: "web/addConsultRecord.php",
+               type: "POST",
+               data: formData,
+               dataType: "json",
+               success: function (data) {
+                   if (data.result == 'SUCCESS') {
+                       alert("상담기록 등록 완료");
+                       $("#consult-record-form")[0].reset();
+                   }
+                   else if (data.result == 'FAIL') {
+                       alert(data.msg);
+                   }
+                   else if (data.result == 'DATA-NOTFOUND') {
+                       alert(data.msg);
+                   }
+               },
+               error: function (jqXHR, textStatus, errorThrown) {
+                   alert("error : " + textStatus + "\n" + errorThrown);
+               }
+           });
+       } else {
+           // 고객번호가 존재하지 않음
+           alert("존재하지 않는 고객번호 입니다.");
+           customerNum.focus();
+       }
+   }
 });
 
 // 고객번호 유무 확인
@@ -122,8 +126,6 @@ function customerNumChk() {
             } else if (data.result == 'NOT_EXISTED') {
                 result = false;
             } else if (data.result == 'DATA-NOTFOUND') {
-                alert(data.msg);
-            } else if (data.result == 'CUSTOMER_NUM_ERROR') {
                 alert(data.msg);
             }
         },
